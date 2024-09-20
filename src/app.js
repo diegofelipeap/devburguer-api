a39d34587e216079a3f41b3e4adcdd285df50fae
diegofelipeap <diegospd22@gmail.com>
2024-09-19 21:10:00 -0300

    Corrigindo configurações do Prettier eEslint.

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ import express from 'express'
 import routes from './routes'
 
 class App {
-    constructor() {
-        this.app = express()
+  constructor() {
+    this.app = express()
 
-        this.middlewares()
-        this.routes()
-    }
+    this.middlewares()
+    this.routes()
+  }
 
-    middlewares() {
-        this.app.use(express.json())
-    }
-    routes() {
-        this.app.use(routes)
-    }
+  middlewares() {
+    this.app.use(express.json())
+  }
+  routes() {
+    this.app.use(routes)
+  }
 }
 
 export default new App().app
